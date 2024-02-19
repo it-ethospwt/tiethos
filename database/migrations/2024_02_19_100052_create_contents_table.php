569a17d03bb0fcb875383d6b41be4c64a3ca66a3
it-ethospwt <it.ethospwt@gmail.com>
2024-02-19 10:06:24 +0700

    tambah migration contents

---
+++ database/migrations/2024_02_19_100052_create_contents_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('contents', function (Blueprint $table) {
+            $table->integer('content_id');
+            $table->bigInteger('product_id');
+            $table->string('title');
+            $table->enum('konten', ['Agency Luar', 'ccp', 'cwm']);
+            $table->string('gambar');
+            $table->string('video');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('contents');
+    }
+};
