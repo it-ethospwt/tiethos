487f294c2cbc58163ad208c5f15928da5c0c1689
Technical Development <it-ethospwt@gmail.com>
2024-03-23 11:15:33 +0700

    penambahan kolom di table user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->bigInteger('tim_id');
             $table->string('name');
             $table->string('email');
             $table->timestamp('email_verified_at')->nullable();
